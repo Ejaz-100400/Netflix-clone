1a092f528ce12a74b7eb862d340500bdbf1bd922
Ejaz-100400 <ejazahmed4688@gmail.com>
2023-02-24 21:07:54 +0530

    backend changes

--- Backend/Routes/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const trendingnowvideotilecontroller =require('../Controllers/Netflix')
-const router = express.Router();
-
-router.get('/shows',trendingnowvideotilecontroller.displaymovie);
-router.get('/shows/:_id',trendingnowvideotilecontroller.playmovietile)
-router.get('/shows/search/:moviename',trendingnowvideotilecontroller.searchmovie)
-module.exports =router;
